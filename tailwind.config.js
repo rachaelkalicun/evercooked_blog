dae4cc56d039424773ba4b88deb07d8e8df840ae
Rachael <rhg108@gmail.com>
2025-02-12 12:55:38 -0700

    move things to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
     "./source/**/*.{html,erb}"
   ],
   theme: {
-    extend: {},
+    extend: {
+			colors: {
+        link: "#5960B2",
+        linkhover: "#363a6d",
+        accent: "#d97706",
+        backgroundlight: "#f4f4f7",
+        lightgray: "#C5C5D2",
+        body: "#171223",
+      },
+			fontFamily: {
+        serif: ["EB Garamond", "serif"],
+        sans: ["Lato", "sans-serif"],
+      },
+		},
   },
   plugins: [],
 }
